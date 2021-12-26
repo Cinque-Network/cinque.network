68b22c54b645d66f0d4be8c42a1f3c5ba5998208
primboi <prianshmittra@gmail.com>
2021-12-26 14:01:27 -0800

    Added navbar and added color scheme script

--- index.php
+++ index.php
@@ -1,7 +1,56 @@
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<header>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+                    <a class="navbar-item">
+                        Blockchain
+                    </a>
+                    <a class="navbar-item">
+                        Currency
+                    </a>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
+                                <script>
+                                    var $color_scheme = Cookies.get("color_scheme");
 
-<body>
-    <?php
+                                    function get_color_scheme() {
+                                        return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
+                                    }
 
-    ?>
+                                    function update_color_scheme() {
+                                        Cookies.set("color_scheme", get_color_scheme());
+                                    }
+                                    // read & compare cookie `color-scheme`
+                                    if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
+                                        update_color_scheme();
+                                    // detect changes and change the cookie
+                                    if (window.matchMedia)
+                                        window.matchMedia("(prefers-color-scheme: dark)").addListener(update_color_scheme);
+                                </script>
+                                <?php
+                                $color_scheme = isset($_COOKIE["color_scheme"]) ? $_COOKIE["color_scheme"] : false;
+                                if ($color_scheme === false) $color_scheme = 'unknown';
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </nav>
+</header>
+
+<style>
+    .background-grey-extralight{
+        background-color: #EAEFF0;
+    }
+</style>
+<body class="background-grey-extralight">
+
 </body>
