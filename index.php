ffbe9f4553a8bbce6eb011ce5b1f0946ccfe588e
primboi <prianshmittra@gmail.com>
2021-12-26 21:01:52 -0800

    started the tech specs

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 <link rel="stylesheet" href="./ext/initial-variables.sass">
 <style>
-    .background-grey-extralight{
+    .background-grey-extralight {
         background-color: #EAEFF0;
     }
-    .blackborder{
-        color: black; 
+
+    .blackborder {
+        color: black;
     }
 </style>
 <header>
@@ -14,56 +15,69 @@
         <div class="navbar-brand">
             <img src="./images/brand.png" width="60px" style="margin:5px">
         </div>
-            <div class="navbar-menu" style="margin:10px;">
-                <div class="navbar-start">
-                    <a class="navbar-item">
-                        <span>Home</span>
-                    </a>
-                    <a class="navbar-item">
-                        <span>Blockchain</span>
-                    </a>
-                    <a class="navbar-item">
-                        <span>Currency</span>
-                    </a>
-                    <div class="navbar-end">
-                        <div class="navbar-item">
-                            <div class="buttons">
-                                <script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
-                                <script>
-                                    var $color_scheme = Cookies.get("color_scheme");
+        <div class="navbar-menu" style="margin:10px;">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    <span>Home</span>
+                </a>
+                <a class="navbar-item">
+                    <span>Blockchain</span>
+                </a>
+                <a class="navbar-item">
+                    <span>Currency</span>
+                </a>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
+                            <script>
+                                var $color_scheme = Cookies.get("color_scheme");
 
-                                    function get_color_scheme() {
-                                        return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
-                                    }
+                                function get_color_scheme() {
+                                    return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
+                                }
 
-                                    function update_color_scheme() {
-                                        Cookies.set("color_scheme", get_color_scheme());
-                                    }
-                                    // read & compare cookie `color-scheme`
-                                    if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
-                                        update_color_scheme();
-                                    // detect changes and change the cookie
-                                    if (window.matchMedia)
-                                        window.matchMedia("(prefers-color-scheme: dark)").addListener(update_color_scheme);
-                                </script>
-                                <?php
-                                $color_scheme = isset($_COOKIE["color_scheme"]) ? $_COOKIE["color_scheme"] : false;
-                                if ($color_scheme === false) $color_scheme = 'unknown';
-                                ?>
-                            </div>
+                                function update_color_scheme() {
+                                    Cookies.set("color_scheme", get_color_scheme());
+                                }
+                                // read & compare cookie `color-scheme`
+                                if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
+                                    update_color_scheme();
+                                // detect changes and change the cookie
+                                if (window.matchMedia)
+                                    window.matchMedia("(prefers-color-scheme: dark)").addListener(update_color_scheme);
+                            </script>
+                            <?php
+                            $color_scheme = isset($_COOKIE["color_scheme"]) ? $_COOKIE["color_scheme"] : false;
+                            if ($color_scheme === false) $color_scheme = 'unknown';
+                            ?>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     </nav>
 </header>
 <br>
 <br>
+
 <body>
-    <p class="title has-text-weight-bold has-text-centered">
-        Welcome to Cinque
-    </p>
-    <div class="container">
-        <hr class="blackborder"></hr>
+    <div style="margin:10px">
+        <p class="title has-text-weight-bold has-text-centered">
+            Welcome to Cinque
+        </p>
+        <div class="container">
+            <hr class="blackborder">
+            </hr>
+        </div>
+        <p class="subtitle">
+            <strong>Technical Specifiactions</strong>
+        </p>
+        <p class="is-size-6">
+            <span class="icon">
+                <i class="far fa-signature"></i>
+            </span>
+            <span>Coin Name: <strong>Cinque</strong></span>
+        </p>
     </div>
 </body>
