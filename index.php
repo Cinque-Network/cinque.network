090f25190cb638e70188c6c48aad0f4cc097d6f3
primboi <prianshmittra@gmail.com>
2021-12-27 11:24:26 -0800

    Created main section and second section (still working on)

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon"> 
 <link rel="stylesheet" href="./ext/initial-variables.sass">
+<meta property="og:site_name" content="CinQue Network" />
+<meta property="og:site" content="https://cinque.network" />
+<meta property="og:title" content="CinQue Network" />
+<meta property="og:description" content="Cinque - A multi-algo fork of digibyte" />
+<meta property="og:image" content="./images/brand.png" />
+<meta property="og:url" content="https://cinque.network" />
+<meta property="og:type" content="website" />
+<title>Cinque Network</title>
 <style>
     .background-grey-extralight {
         background-color: #EAEFF0;
@@ -9,49 +18,52 @@
     .blackborder {
         color: black;
     }
+
+    hr {
+        background-color: grey;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .hr2 {
+        width: 20%;
+        margin-left: 10px;
+        background-color: black;
+    }
+    .hr3 {
+        width: 30%;
+        height: 5px;
+        margin-left: auto;
+        margin-right: auto;
+        background-color:aqua;
+    }
 </style>
 <header>
-    <nav class="navbar background-grey-extralight" role="navigation" aria-label="main navigation">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <img src="./images/brand.png" width="60px" style="margin:5px">
+            <a class="navbar-item is-active" href="/">
+                <img src="./images/brand.png" height="172">
+                &nbsp;Home
+            </a>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+                <span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span>
+            </a>
         </div>
-        <div class="navbar-menu" style="margin:10px;">
+        <div id="navbar" class="navbar-menu">
             <div class="navbar-start">
-                <a class="navbar-item">
-                    <span>Home</span>
+                <a class="navbar-item" href="#">
+                    <i class="fas fa-book" style="color:#f6b93b;"></i>&nbsp;Whitepaper
                 </a>
-                <a class="navbar-item">
-                    <span>Blockchain</span>
+                <a class="navbar-item" href="#">
+                    <i class="fas fa-heart" style="color:#ff7474;"></i>&nbsp;Donate
                 </a>
-                <a class="navbar-item">
-                    <span>Currency</span>
+                <a class="navbar-item" href="#">
+                    <i class="fas fa-user" style="color:#00e7ff;"></i>&nbsp;Our Team
                 </a>
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
-                            <script>
-                                var $color_scheme = Cookies.get("color_scheme");
-
-                                function get_color_scheme() {
-                                    return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
-                                }
-
-                                function update_color_scheme() {
-                                    Cookies.set("color_scheme", get_color_scheme());
-                                }
-                                // read & compare cookie `color-scheme`
-                                if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
-                                    update_color_scheme();
-                                // detect changes and change the cookie
-                                if (window.matchMedia)
-                                    window.matchMedia("(prefers-color-scheme: dark)").addListener(update_color_scheme);
-                            </script>
-                            <?php
-                            $color_scheme = isset($_COOKIE["color_scheme"]) ? $_COOKIE["color_scheme"] : false;
-                            if ($color_scheme === false) $color_scheme = 'unknown';
-                            ?>
-                        </div>
+            </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
                     </div>
                 </div>
             </div>
@@ -62,22 +74,69 @@
 <br>
 
 <body>
-    <div style="margin:10px">
-        <p class="title has-text-weight-bold has-text-centered">
-            Welcome to Cinque
+    <section>
+        <p class="title has-text-centered">
+            Cinque
         </p>
         <div class="container">
-            <hr class="blackborder">
-            </hr>
+            <hr>
         </div>
-        <p class="subtitle">
-            <strong>Technical Specifictions</strong>
+        <p class="is-size-5 has-text-centered" style="margin-left:30px;">
+            A decentralized coin <b>forked</b> from digibyte with <b>5</b> PoW (<b>Proof of Work</b>) algorithms. 
         </p>
-        <p class="is-size-6">
-            <span class="icon">
-                <i class="far fa-signature"></i>
-            </span>
-            <span>Coin Name: <strong>Cinque</strong></span>
+        <br>
+        <p class="is-size-4 has-text-centered">
+            <b>So, what are the benifts of joining Cinque Network?</b>
         </p>
-    </div>
+        <div class="columns" style="margin:10px;">
+            <div class="column is-size-5">
+                <ul class="is-shadow">
+                    <li><span class="icon">
+                        <i class="far fa-signature"></i>
+                    </span>
+                    <span>Coin Name: <strong>Cinque</strong></span></li>
+                    <li><span class="icon"><i class="fas fa-sticky-note"></i></span>
+                    <span>Ticker: <strong>CIN</strong></span></li>
+                    <li><span class="icon"><i class="fas fa-coins"></i></span>
+                    <span>Max Coins: <strong>5 Million</strong></span></li>
+                    <li><span class="icon"><i class="fas fa-stopwatch"></i></span>
+                    <span>Block Time: <strong>10 Minutes</strong></span></li>
+                    <li><span class="icon"><i class="fas fa-code"></i></span>
+                    <span><span>Algorithms(PoW):</span> <span class="is-size-6"><br><b>Cuckatoo32</b> (GPU, ASIC),<br> <b>MYR-GRS</b>(ASIC), <br><b>Ghostrider</b> (CPU, GPU), <br><b>Keccak</b>(CPU, GPU, ASIC, FGPA) <br><b>HeavyHash</b>(CPU, GPU, FGPA, ASIC)</span></span>
+                </ul>
+            </div>
+            <div class="column is-two-thirds is-size-5">
+                <ul class="is-shadow">
+                    <b>Why Else?</b>
+                    <li><span class="icon"><i class="fas fa-desktop"></i></span>
+                    <span>On going development</span></li>
+                    <li><span class="icon"><i class="far fa-balance-scale"></i></span>
+                    <span>Balanced Algorithms</span></li>
+                    <li><span class="icon"><i class="far fa-book-open"></i></span>
+                    <span>Open Source</span></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="has-background-link has-text-white">
+        <p class="has-text-centered title has-text-white">
+            <br>
+            Coin Information
+        </p>
+        <div class="container">
+            <hr class="hr3">
+        </div>
+        <div class="columns">
+            <div class="column is-two-thirds is-size-5">
+                <ul class="is-shadow">
+                    <li style="margin-left:20px;">
+                        <span class="icon">
+                            <i class="fas fa-code-branch"></i>
+                        </span>
+                        <span><a class="has-text-white">DGB</a>-CIN hardfork</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </body>
