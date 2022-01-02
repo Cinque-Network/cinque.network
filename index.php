921c07aa8210cc52b106c869a3b614d966b98ea4
primboi <prianshmittra@gmail.com>
2022-01-01 16:40:08 -0800

    added a form in (jotform)

--- index.php
+++ index.php
@@ -293,14 +293,228 @@
         <br>
         <p class="has-text-centered subtitle has-text-white">
         </p>
-        <div class="has-text-centered">
-            <form action="getexchanges.php" method="get">
-                Email: <input type="text" name="name"><br>
-                Exchange: <input type="text" name="email"><br>
-                <br>
-                <input class="button is-rounded" type="submit">
-            </form>
-        </div>
+        <script src="https://cdn01.jotfor.ms/static/prototype.forms.js" type="text/javascript"></script>
+        <script src="https://cdn02.jotfor.ms/static/jotform.forms.js?3.3.29871" type="text/javascript"></script>
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/punycode/1.4.1/punycode.js"></script>
+        <script type="text/javascript">
+            JotForm.newDefaultTheme = true;
+            JotForm.extendsNewTheme = false;
+            JotForm.newPaymentUIForNewCreatedForms = true;
+            JotForm.newPaymentUI = true;
+            JotForm.clearFieldOnHide = "disable";
+            JotForm.submitError = "jumpToFirstError";
+
+            JotForm.init(function() {
+                /*INIT-START*/
+                if (window.JotForm && JotForm.accessible) $('input_4').setAttribute('tabindex', 0);
+                JotForm.alterTexts(undefined);
+                /*INIT-END*/
+            });
+
+            JotForm.prepareCalculationsOnTheFly([null, {
+                "name": "heading",
+                "qid": "1",
+                "text": "Exchange Request",
+                "type": "control_head"
+            }, {
+                "name": "submit2",
+                "qid": "2",
+                "text": "Submit",
+                "type": "control_button"
+            }, {
+                "description": "",
+                "name": "email",
+                "qid": "3",
+                "subLabel": "example@example.com",
+                "text": "Email",
+                "type": "control_email"
+            }, {
+                "description": "",
+                "name": "typeA",
+                "qid": "4",
+                "subLabel": "eg. binance.com",
+                "text": "Enter Exchange:",
+                "type": "control_textbox"
+            }, {
+                "description": "",
+                "name": "pleaseVerify",
+                "qid": "5",
+                "text": "Please verify that you are human",
+                "type": "control_captcha"
+            }]);
+            setTimeout(function() {
+                JotForm.paymentExtrasOnTheFly([null, {
+                    "name": "heading",
+                    "qid": "1",
+                    "text": "Exchange Request",
+                    "type": "control_head"
+                }, {
+                    "name": "submit2",
+                    "qid": "2",
+                    "text": "Submit",
+                    "type": "control_button"
+                }, {
+                    "description": "",
+                    "name": "email",
+                    "qid": "3",
+                    "subLabel": "example@example.com",
+                    "text": "Email",
+                    "type": "control_email"
+                }, {
+                    "description": "",
+                    "name": "typeA",
+                    "qid": "4",
+                    "subLabel": "eg. binance.com",
+                    "text": "Enter Exchange:",
+                    "type": "control_textbox"
+                }, {
+                    "description": "",
+                    "name": "pleaseVerify",
+                    "qid": "5",
+                    "text": "Please verify that you are human",
+                    "type": "control_captcha"
+                }]);
+            }, 20);
+        </script>
+        <style type="text/css">
+            @media print {
+                .form-section {
+                    display: inline !important
+                }
+
+                .form-pagebreak {
+                    display: none !important
+                }
+
+                .form-section-closed {
+                    height: auto !important
+                }
+
+                .page-section {
+                    position: initial !important
+                }
+            }
+        </style>
+        <link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?themeRevisionID=5f7ed99c2c2c7240ba580251" />
+        <link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/css/styles/payment/payment_styles.css?3.3.29871" />
+        <link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/css/styles/payment/payment_feature.css?3.3.29871" />
+        <form class="jotform-form" action="https://submit.jotform.com/submit/220007055718044/" method="post" name="form_220007055718044" id="220007055718044" accept-charset="utf-8" autocomplete="on">
+            <input type="hidden" name="formID" value="220007055718044" />
+            <input type="hidden" id="JWTContainer" value="" />
+            <input type="hidden" id="cardinalOrderNumber" value="" />
+            <div role="main" class="form-all">
+                <style>
+                    .form-all:before {
+                        background: none;
+                    }
+                </style>
+                <ul class="form-section page-section">
+                    <li id="cid_1" class="form-input-wide" data-type="control_head">
+                        <div class="form-header-group  header-large">
+                            <div class="header-text httal htvam">
+                                <h1 id="header_1" class="form-header" data-component="header">
+                                    Exchange Request
+                                </h1>
+                                <div id="subHeader_1" class="form-subHeader">
+                                    Put an exchange link in!
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="form-line jf-required" data-type="control_email" id="id_3">
+                        <label class="form-label form-label-top form-label-auto" id="label_3" for="input_3">
+                            Email
+                            <span class="form-required">
+                                *
+                            </span>
+                        </label>
+                        <div id="cid_3" class="form-input-wide jf-required" data-layout="half">
+                            <span class="form-sub-label-container" style="vertical-align:top">
+                                <input type="email" id="input_3" name="q3_email" class="form-textbox validate[required, Email]" data-defaultvalue="" style="width:310px" size="310" value="" data-component="email" aria-labelledby="label_3 sublabel_input_3" required="" />
+                                <label class="form-sub-label" for="input_3" id="sublabel_input_3" style="min-height:13px" aria-hidden="false"> example@example.com </label>
+                            </span>
+                        </div>
+                    </li>
+                    <li class="form-line jf-required" data-type="control_textbox" id="id_4">
+                        <label class="form-label form-label-top form-label-auto" id="label_4" for="input_4">
+                            Enter Exchange:
+                            <span class="form-required">
+                                *
+                            </span>
+                        </label>
+                        <div id="cid_4" class="form-input-wide jf-required" data-layout="half">
+                            <span class="form-sub-label-container" style="vertical-align:top">
+                                <input type="text" id="input_4" name="q4_typeA" data-type="input-textbox" class="form-textbox validate[required]" data-defaultvalue="" style="width:310px" size="310" value="" data-component="textbox" aria-labelledby="label_4 sublabel_input_4" required="" />
+                                <label class="form-sub-label" for="input_4" id="sublabel_input_4" style="min-height:13px" aria-hidden="false"> eg. binance.com </label>
+                            </span>
+                        </div>
+                    </li>
+                    <li class="form-line jf-required" data-type="control_captcha" id="id_5">
+                        <label class="form-label form-label-top form-label-auto" id="label_5" for="input_5">
+                            Please verify that you are human
+                            <span class="form-required">
+                                *
+                            </span>
+                        </label>
+                        <div id="cid_5" class="form-input-wide jf-required" data-layout="full">
+                            <section data-wrapper-react="true">
+                                <div id="hcaptcha_input_5" class="h-captcha" data-siteKey="772f4a50-7161-425e-8cd5-4d7e361ab765" data-callback="hcaptchaCallbackinput_5" data-expired-callback="hcaptchaExpiredCallbackinput_5">
+                                </div>
+                                <input type="hidden" id="input_5" class="hidden validate[required]" name="hcaptcha_visible" required="" />
+                                <script type="text/javascript" src="https://hcaptcha.com/1/api.js"></script>
+                                <script type="text/javascript">
+                                    var hcaptchaCallbackinput_5 = function(token) {
+                                        var hiddenInput = $("input_5");
+                                        hiddenInput.setValue(1);
+                                        if (hiddenInput.validateInput) {
+                                            hiddenInput.validateInput();
+                                        }
+                                    }
+
+                                    var hcaptchaExpiredCallbackinput_5 = function() {
+                                        var hiddenInput = $("input_5");
+                                        hiddenInput.writeAttribute("value", false);
+                                        if (hiddenInput.validateInput) {
+                                            hiddenInput.validateInput();
+                                        }
+                                    }
+                                </script>
+                            </section>
+                        </div>
+                    </li>
+                    <li class="form-line" data-type="control_button" id="id_2">
+                        <div id="cid_2" class="form-input-wide" data-layout="full">
+                            <div data-align="auto" class="form-buttons-wrapper form-buttons-auto   jsTest-button-wrapperField">
+                                <button id="input_2" type="submit" class="form-submit-button submit-button jf-form-buttons jsTest-submitField" data-component="button" data-content="">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li style="display:none">
+                        Should be Empty:
+                        <input type="text" name="website" value="" />
+                    </li>
+                </ul>
+            </div>
+            <script>
+                JotForm.showJotFormPowered = "new_footer";
+            </script>
+            <script>
+                JotForm.poweredByText = "Powered by Jotform";
+            </script>
+            <input type="hidden" class="simple_spc" id="simple_spc" name="simple_spc" value="220007055718044" />
+            <script type="text/javascript">
+                var all_spc = document.querySelectorAll("form[id='220007055718044'] .si" + "mple" + "_spc");
+                for (var i = 0; i < all_spc.length; i++) {
+                    all_spc[i].value = "220007055718044-220007055718044";
+                }
+            </script>
+            <div class="formFooter-heightMask">
+            </div>
+        </form>
+        <script src="https://cdn.jotfor.ms//js/vendor/smoothscroll.min.js?v=3.3.29871"></script>
+        <script src="https://cdn.jotfor.ms//js/errorNavigation.js?v=3.3.29871"></script>
         <br>
     </section>
 </body>
